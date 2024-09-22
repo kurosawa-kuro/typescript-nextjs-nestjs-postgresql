a049b3ba522db840e309b7eda3ab298c7bb65685
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-09-22 21:05:00 +0900

    Refactor import paths for category and micropost modules and add DatabaseModule

--- backend/src/category/category.module.ts
+++ backend/src/category/category.module.ts
@@ -3,9 +3,10 @@ import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 import { MicropostCategoryService } from '../micropost-category/micropost-category.service';
 import { MicropostCategoryModule } from '../micropost-category/micropost-category.module';
+import { DatabaseModule } from '../database/database.module'; // Add this import
 
 @Module({
-  imports: [MicropostCategoryModule],
+  imports: [MicropostCategoryModule, DatabaseModule], // Add DatabaseModule here
   controllers: [CategoryController],
   providers: [CategoryService, MicropostCategoryService],
 })
--- backend/src/micropost/micropost.module.ts
+++ backend/src/micropost/micropost.module.ts
@@ -1,12 +1,12 @@
-// src/micropost/micropost.module.ts
-
 import { Module } from '@nestjs/common';
 import { MicroPostService } from './micropost.service';
 import { MicroPostController } from './micropost.controller';
 import { DatabaseModule } from '../database/database.module';
+import { UserModule } from '../user/user.module';
+import { MicropostCategoryModule } from '../micropost-category/micropost-category.module'; // Import MicropostCategoryModule to access MicropostCategoryService
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UserModule, MicropostCategoryModule], // Add MicropostCategoryModule here
   providers: [MicroPostService],
   controllers: [MicroPostController],
   exports: [MicroPostService],
