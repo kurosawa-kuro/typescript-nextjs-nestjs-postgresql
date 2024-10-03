953522f96f10994983647a1213ae7ed6154b0d1e
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-10-03 21:30:25 +0900

    Revert "Refactor API URL handling in ApiService"
    This reverts commit 5777a503cf43174deb87455008e020d4e0ce5d52.

--- frontend/src/app/api/apiService.ts
+++ frontend/src/app/api/apiService.ts
@@ -2,25 +2,22 @@
 
 import { Micropost, LoginResponse } from '../types';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+const API_URL = 'http://localhost:3001/microposts';
 
 export const ApiService = {
   fetchMicroposts: async (): Promise<Micropost[]> => {
-    const response = await fetch(`${API_URL}/microposts`);
-
+    const response = await fetch(API_URL);
     if (!response.ok) {
       throw new Error('Failed to fetch microposts');
     }
-
     return response.json();
   },
 
   createMicropost: async (formData: FormData): Promise<Micropost> => {
-    const response = await fetch(`${API_URL}/microposts`, {
+    const response = await fetch(API_URL, {
       method: 'POST',
       body: formData,
     });
-    console.log('response' ,response);
     if (!response.ok) {
       throw new Error('Failed to create micropost');
     }
@@ -29,7 +26,7 @@ export const ApiService = {
   },
 
   login: async (email: string, password: string): Promise<LoginResponse> => {
-    const response = await fetch(`${API_URL}/auth/login`, {
+    const response = await fetch('http://localhost:3001/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
