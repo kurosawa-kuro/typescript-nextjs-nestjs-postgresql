3b2b92d85a33abe54b18065acfa1e46fd4bb66eb
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-10-04 21:01:44 +0900

    Refactor usePostForm to remove unused code and improve code organization

--- frontend/src/app/hooks/usePostForm.ts
+++ frontend/src/app/hooks/usePostForm.ts
@@ -2,14 +2,12 @@ import { useState } from 'react';
 
 export const usePostForm = () => {
   const [formTitle, setFormTitle] = useState("");
-  const [formContent, setFormContent] = useState("");
   const [formImage, setFormImage] = useState<File | null>(null);
 
   const resetForm = () => {
     setFormTitle("");
-    setFormContent("");
     setFormImage(null);
   };
 
-  return { formTitle, setFormTitle, formContent, setFormContent, formImage, setFormImage, resetForm };
+  return { formTitle, setFormTitle, formImage, setFormImage, resetForm };
 };
